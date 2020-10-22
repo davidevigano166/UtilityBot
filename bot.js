e9471ef048d4250eaa0f26a3bc819356ac8d4ef6
davidevigano166 <dadaviga@gmail.com>
2020-10-22 14:09:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,46 +4,56 @@ const Discord = require('discord.js');
 const { exit } = require('process');
 const client = new Discord.Client();
 
-const commands = {
+const commands = { // Commands
 	cmdhelp: 'help',
+  cmdmembers: 'members',
   cmdinfo: 'info',
-  cmdtime: 'time'
+  cmdtime: 'time',
+  cmddate: 'date'
 }
 
-client.on('ready', () => {
+client.on('ready', () => { // Initial Function
   console.log("Logged in as " + client.user.tag);
-  client.user.setStatus('online');
-  client.user.setPresence({ activity: { name: 'use <help'}, status: 'online'});
+  client.user.setPresence({ activity: { name: 'use $help'}, status: 'online'});
 });
 
-client.on('message', msg => {
-  if (msg.content.substring(0, 1) === '<') {
+client.on('message', msg => { // Replies
+  if (msg.content.substring(0, 1) === '$') {
 	  let command = msg.content.substring(1);
 	  switch (command) {
 		  case commands.cmdhelp:
-        let str = 'these are the commands available: ';
+        let str = 'Available commands: \n';
         for (var x in commands) {
-          str += commands[x] + ', ';
+          str += '$' + commands[x] + '\n';
         }
-		  	msg.reply(str);
+		  	msg.channel.send(str);
 			  break;
-		  case commands.cmdinfo:
-        let guildvar = client.guilds.fetch('768524986905526342').then(guild => msg.reply('total members: ' + guild.memberCount));
+		  case commands.cmdmembers:
+        let guildvar = client.guilds.fetch('768524986905526342').then(guild => msg.channel.send('There are ' + guild.memberCount + ' members'));
+        break;
+      case commands.cmdinfo:
+        let namevar = client.guilds.fetch('768524986905526342').then(guild => msg.channel.send('Server name: ' + guild.name));
         break;
       case commands.cmdtime:
         let timevar = new Date();
-        msg.reply('the time is ' + timevar.getHours() + ':' + timevar.getMinutes() + ':' + timevar.getSeconds());
+        msg.channel.send('It is ' + timevar.getHours() + ':' + timevar.getMinutes() + ':' + timevar.getSeconds());
+        break;
+      case commands.cmddate:
+        let datevar = new Date();
+        let month = parseInt(datevar.getMonth())+1;
+        msg.channel.send('Today is ' + datevar.getDate() + '-' + month + '-' + datevar.getFullYear());
+        break;
 	  }
   }
 });
 
-client.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', member => { // Welcome Message
     const channel = member.guild.channels.cache.find(ch => ch.name === 'generale');
     if (!channel) return;
     channel.send(`Benvenuto nel server, ${member}!`);
 });
 
-function startUp() {
+function startUp() { // StartUp Function
     try {
         var tokenauth = fs.readFileSync('./auth.json');
         tokenauth = JSON.parse(tokenauth).token;
